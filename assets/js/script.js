999e759ed0ad6844e8e8d58b253369b0f0fbd7b4
tiagorb03 <tiagorb03@gmail.com>
2026-04-13 20:26:49 -0300

    js

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,6 +19,6 @@ setInterval(() => {
 
 }, 1000);
 
-const campo = document.getElementById('outro');
+const campo = document.getElementById("outro");
 campo.disabled = true;  // Desabilita
 campo.disabled = false; // Habilita
