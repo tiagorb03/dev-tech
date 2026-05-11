ba0327bef3709376d0639a04a1f311bc5e5eea1a
tiagorb03 <tiagorb03@gmail.com>
2026-05-11 19:36:58 -0300

    js relatorio

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,3 +31,48 @@ function toggleCampo() {
     
     campo.disabled = !checkbox.checked;
 }
+
+async function carregarInscritos() {
+
+    const { data, error } = await supabaseClient
+
+        .from("Cadastro")
+
+        .select("*");
+
+    console.log(data);
+
+    console.log(error);
+
+    if (error) {
+
+        console.error(error);
+
+        alert(error.message);
+
+        return;
+    }
+
+    const tabela =
+        document.getElementById("tabelaInscritos");
+
+    if (!tabela) return;
+
+    tabela.innerHTML = "";
+
+    data.forEach(inscrito => {
+
+        tabela.innerHTML += `
+            <tr>
+                <td>${inscrito.nome ?? ""}</td>
+                <td>${inscrito.email ?? ""}</td>
+                <td>${inscrito.telefone ?? ""}</td>
+                <td>${inscrito.atuacao ?? ""}</td>
+                <td>${inscrito.interesse ?? ""}</td>
+                <td>${inscrito.data ?? ""}</td>
+
+            </tr>
+        `;
+
+    });
+}
