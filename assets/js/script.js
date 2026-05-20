90ccd24d95b8bdb8820cbfac6382b3128e8c2917
tiagorb03 <tiagorb03@gmail.com>
2026-05-19 22:54:36 -0300

    sds

--- assets/js/script.js
+++ assets/js/script.js
@@ -135,13 +135,13 @@ async function carregarInscritos() {
         // Botão Cancelar
         const btnCancelar = document.createElement("button");
         btnCancelar.id = `btnCancelar-${inscrito.id}`;
+        btnCancelar.style.display = "none";
         btnCancelar.innerHTML = `
             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" viewBox="0 0 16 16">
                 <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
             </svg>
         `;
         btnCancelar.onclick = () => cancelarEdicao(inscrito.id);
-        btnCancelar.style.display = "none";
 
         tdAcoes.appendChild(btnEditar);
         tdAcoes.appendChild(btnExcluir);
